bcc93b6b60eafd15e72bb364a6d5ba00004ff24a
mih0100 <kamily.vitoria.sousa@escola.pr.gov.br>
2025-09-01 09:44:49 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,6 +4,12 @@
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você`
+    const container = document.getElementById("graficos-container")
+    container.appendChild(paragrafo)
  }
 
  vizualizarInformacaoGlobais();
